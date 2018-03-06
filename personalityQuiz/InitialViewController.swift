b324acf333ba40311633defa7a923cd4da1d1324
Claire Lutz <clutz9236@jh-mbas-clutz9236.local>
2018-03-06 10:54:36 -0600

    added subviews and stuff

--- personalityQuiz/InitialViewController.swift
+++ personalityQuiz/InitialViewController.swift
@@ -8,18 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class InitialViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBAction func myButton(_ sender: UIButton)
+    {
+        performSegue(withIdentifier: "resultsSegue", sender: nil)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
 
 }
 
